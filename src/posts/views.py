f5a2274c74cce93cfd4d08150b2386b36bc3bb31
Luis Rosales <eng.luisrosales@gmail.com>
2018-01-03 19:17:14 +0100

    adding some templates and routes to see list of posts

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from posts.models import Post
+
+
+@login_required
+def home(request):
+    latest_posts = Post.objects.all().order_by('-published_date')
+    context = { 'posts': latest_posts[:10] }
+    return render(request, "home.html", context)
+
+
+class MyPostsView(LoginRequiredMixin,ListView):
+    model = Post
+    template_name = "my_posts.html"
+
+    def get_queryset(self):
+        queryset = super(MyPostsView, self).get_queryset()
+        return queryset.filter(user=self.request.user)
--- src/wordplease/urls.py
+++ src/wordplease/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, logout
 from django.urls import path
+
+from posts.views import MyPostsView, home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('login', LoginView.as_view(), name="login_page"),
+    path('logout', logout, name="logout_page"),
+
+    # path('posts/crear', CreateMovieView.as_view(), name="create_movie_page"),
+    # path('pelis/<int:pk>', movie_detail, name="movie_detail_page"),
+    path('posts/', MyPostsView.as_view(), name='my_posts_page'),
+    path('', home, name="home_page"),
 ]
