0684194290c573b0cce58d213515d2ced44e5e81
Luis Rosales <eng.luisrosales@gmail.com>
2018-01-04 18:28:12 +0100

    fixing bug

--- src/users/api.py
+++ src/users/api.py
@@ -43,7 +43,7 @@ class UserDetailAPI(APIView):
         return Response(serializer.data)
 
     def put(self, request, pk):
-        user = get_object_or_404(User, pk=pk)
+        user = get_object_or_404(User, username=pk)
         self.check_object_permissions(request, user)
         serializer = UserSerializer(user, data=request.data)
         if serializer.is_valid():
@@ -53,7 +53,7 @@ class UserDetailAPI(APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
-        user = get_object_or_404(User, pk=pk)
+        user = get_object_or_404(User, username=pk)
         self.check_object_permissions(request, user)
         user.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
